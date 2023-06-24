c2394a406c185965fd2354d0356b27586e43afd2
Sreekar <satyasreekarpattaswami@gmail.com>
2023-06-24 14:09:11 +0530

    Form functionality

--- Select.php
+++ Select.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-if(!isset($_SESSION['UN']))
-{
+if (!isset($_SESSION['UN'])) {
     header("location:index.php");
     exit;
 }
 ?>
+
 <html>
 
 <head>
@@ -17,36 +17,40 @@ if(!isset($_SESSION['UN']))
 <body>
     <div class="heading">
         <img src="logo.jpg" alt="">
-            <h1>Digital Trainer</h1>
-            <button onclick="window.location.href = 'logout.php';">Logout</button>
+        <h1>Digital Trainer</h1>
+        <button onclick="window.location.href = 'logout.php';">Logout</button>
     </div>
     <div class="log">
         <div class="login">
             <center>
                 <h2>Select Muscle Group</h2>
-            <table>
-                    <tr>
-                    <th><button>Shoulder</button></th>
-                    <th><button>Chest</button></th>
-                    </tr>
-                    <tr>
-                    <th><button>Bicep</button></th>
-                    <th><button>Tricep</button></th>
-                    </tr>
-                    <tr>
-                    <th><button>Legs</button></th>
-                    <th><button>Abdomen</button></th>
-                    </tr>
-                    <tr>
-                    <th><button>Circuit</button></th>
-                    <th><button>General</button></th>
-                    </tr>
-            </table>
+                <form action="process.php" method="POST">
+                    <table>
+                        <tr>
+                            <th><button type="submit" name="msl" value="shoulder">Shoulder</button></th>
+                            <th><button type="submit" name="msl" value="chest">Chest</button></th>
+                        </tr>
+                        <tr>
+                            <th><button type="submit" name="msl" value="bicep">Bicep</button></th>
+                            <th><button type="submit" name="msl" value="tricep">Tricep</button></th>
+                        </tr>
+                        <tr>
+                            <th><button type="submit" name="msl" value="legs">Legs</button></th>
+                            <th><button type="submit" name="msl" value="abdomen">Abdomen</button></th>
+                        </tr>
+                        <tr>
+                            <th><button type="submit" name="msl" value="circuit">Circuit</button></th>
+                            <th><button type="submit" name="msl" value="general">General</button></th>
+                        </tr>
+                    </table>
+                </form>
             </center>
         </div>
     </div>
 </body>
+
 <footer>
-    <h3>&copy;Developed by Satya Sreekar Pattaswami</h3>
+    <h3>&copy; Developed by Satya Sreekar Pattaswami</h3>
 </footer>
+
 </html>
